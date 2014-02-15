1e57337bc08f537d8085de6506f6439ce00dc264
Matthew Lewis <matt@mplewis.com>
2014-02-15 17:14:24 -0500

    Allow Content-Type

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(express.bodyParser());
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
   next();
 });
 
